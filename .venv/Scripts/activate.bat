3ee0e2e90501036d32c6fd167288e89fca22d6f1
Denis Vasin <svga.shop@gmail.com>
2022-08-18 12:37:15 +0300

    django

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Code\Project\WEB\django_project\.venv
+set VIRTUAL_ENV=C:\Dev\Projects\django\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- pages/tests.py
+++ pages/tests.py
@@ -35,3 +35,22 @@ class AboutpageTests(SimpleTestCase):
     def test_template_content(self):
         response = self.client.get(reverse("about"))
         self.assertContains(response, "<h1>About page</h1>")
+
+class ContactspageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/contacts/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("contacts"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("contacts"))
+        self.assertTemplateUsed(response, "contacts.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("contacts"))
+        self.assertContains(response, "<h1>Contacts</h1>")
+        self.assertContains(response, '<input type="text" id="Name" name="Name">')
+        self.assertContains(response, '<textarea name="Text" id="Text" cols="30" rows="10">')
