14857e74082a2cdefe0334fefd3702e77fd4f621
Denis Vasin <svga.shop@gmail.com>
2022-08-18 19:22:33 +0300

    django_project posts

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include # new
  
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("pages.urls")), # new
+    path("", include("pages.urls")),
+    path("posts", include("posts.urls"))
 ]
 
