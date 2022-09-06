43ad52c610884fa89d1b28797dddbeab87f54877
Denis Vasin <svga.shop@gmail.com>
2022-09-06 18:45:53 +0300

    Сss

--- message/urls.py
+++ message/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import MessagePageView, MessageDetailView
+from .views import MessagePageView, MessageDetailView, MessageCreateView, MessageUpdateview, MessageDeleteView
  
 urlpatterns = [
     path("", MessagePageView.as_view(), name="message"),
     path("message/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
+    path("mesasge/new/", MessageCreateView.as_view(), name="message_new"),
+    path("mesasge/<int:pk>/edit/", MessageUpdateview.as_view(), name="message_edit"),
+    path("mesasge/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete"),
 ]
