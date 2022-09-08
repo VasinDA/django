acd4e7d4b1cb1aaf6cbd910f2c301a838473b4f1
Denis Vasin <svga.shop@gmail.com>
2022-09-08 18:19:41 +0300

    delete 404

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dhd1i+m-civ4h=w0#hf5pq8i8h+bn!khf&#zoc-i+e)sz_*^ne'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
