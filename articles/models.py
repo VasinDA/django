7d6112d03f3bb61312539de7c4604b640080cf0f
Denis Vasin <svga.shop@gmail.com>
2022-09-16 20:28:25 +0300

    commets

--- articles/models.py
+++ articles/models.py
@@ -12,3 +12,15 @@ class Article(models.Model):
  
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"pk":self.pk})
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+   
+    def __str__(self):
+        return self.comment
+   
+    def get_absolute_url(self):
+        return reverse("article_list")
