d2c9bd1da0c7bf4259369ea76d903ba17af01767
Denis Vasin <svga.shop@gmail.com>
2022-09-16 18:45:58 +0300

    views

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Article
  
@@ -11,17 +12,25 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = "article_detail.html"
 
-class ArticleCreatelView(CreateView):
+class ArticleCreatelView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
-    fields = ["title", "body", "author"]
+    fields = ["title", "body"]
 
-class ArticleUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ArticleUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
     model = Article
     template_name = "article_edit.html"
     fields = ["title", "body"]
 
-class ArticleDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
